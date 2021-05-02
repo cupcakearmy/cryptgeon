5c5e308b158518b66632687be67083fa344385cc
cupcakearmy <hi@nicco.io>
2021-05-02 12:31:40 +0200

    spa mode

--- client/svelte.config.cjs
+++ client/svelte.config.cjs
@@ -1,4 +1,5 @@
-const preprocess = require('svelte-preprocess');
+const preprocess = require('svelte-preprocess')
+const adapter = require('@sveltejs/adapter-static')
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -7,7 +8,10 @@ module.exports = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: adapter({
+			fallback: 'index.html',
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+		target: '#svelte',
+	},
+}
