a040ad469ef276bfe8fffea4d044c9b566da8f21
cupcakearmy <hi@nicco.io>
2021-12-20 17:54:09 +0100

    remove println

--- src/size.rs
+++ src/size.rs
@@ -3,8 +3,7 @@ use byte_unit::Byte;
 
 pub fn init(cfg: &mut web::ServiceConfig) {
   let limit_string = std::env::var("SIZE_LIMIT").unwrap_or("1 KiB".to_string());
-  let limit = Byte::from_str(limit_string.clone()).unwrap().get_bytes() as usize;
-  println!("SIZE_LIMIT: {}, {}", limit_string, limit);
+  let limit = Byte::from_str(limit_string).unwrap().get_bytes() as usize;
   let config = web::JsonConfig::default().limit(limit);
   cfg.data(config);
 }
