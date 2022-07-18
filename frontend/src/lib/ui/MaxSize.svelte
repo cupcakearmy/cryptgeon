15db684a93575405e735cac83d3684ac45fcb123
cupcakearmy <hi@nicco.io>
2022-07-18 23:48:55 +0200

    show size with overhead

--- frontend/src/lib/ui/MaxSize.svelte
+++ frontend/src/lib/ui/MaxSize.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-	import { status } from '$lib/stores/status'
 	import prettyBytes from 'pretty-bytes'
 	import { _ } from 'svelte-intl-precompile'
+
+	import { status } from '$lib/stores/status'
+
+	// Due to encoding overhead (~35%) with base64
+	// https://en.wikipedia.org/wiki/Base64
+	const overhead = 1 / 1.35
 </script>
 
 <span>
 	{#if $status !== null}
-		{prettyBytes($status.max_size, { binary: true })}
+		{prettyBytes($status.max_size * overhead, { binary: true })}
 	{:else}
 		{$_('common.loading')}
 	{/if}
