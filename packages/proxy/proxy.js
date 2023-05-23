b05841a562bbda16c5e3619d26b2f37eeb7f8af1
Niccolo Borgioli <hi@nicco.io>
2023-05-23 09:37:33 +0200

    add timeout

--- packages/proxy/proxy.js
+++ packages/proxy/proxy.js
@@ -10,7 +10,7 @@ proxy.on('error', function (err, req, res) {
 
 const server = http.createServer(function (req, res) {
   const target = req.url.startsWith('/api/') ? 'http://127.0.0.1:8000' : 'http://localhost:8001'
-  proxy.web(req, res, { target })
+  proxy.web(req, res, { target, proxyTimeout: 250, timeout: 250 })
 })
 server.listen(1234)
 console.log('Proxy on http://localhost:1234')
