eb76fe085a0503ce7fb019acc6697d83d00dde26
Niccolo Borgioli <hi@nicco.io>
2024-03-04 18:32:03 +0100

    rm pkg and update node version

--- packages/cli/scripts/build.js
+++ packages/cli/scripts/build.js
@@ -3,29 +3,32 @@
 import { build, context } from 'esbuild'
 import pkg from '../package.json' assert { type: 'json' }
 
-const options = {
-  entryPoints: ['./src/cli.ts'],
+const common = {
   bundle: true,
   minify: true,
   platform: 'node',
-  outfile: './dist/cli.cjs',
   define: { VERSION: `"${pkg.version}"` },
+}
+
+const cliOptions = {
+  ...common,
+  entryPoints: ['./src/cli.ts'],
+  format: 'cjs',
+  outfile: './dist/cli.cjs',
+}
+
+const indexOptions = {
+  ...common,
+  entryPoints: ['./src/index.ts'],
+  outfile: './dist/index.mjs',
+  format: 'esm',
 }
 
 const watch = process.argv.slice(2)[0] === '--watch'
 if (watch) {
-  ;(await context(options)).watch()
+  const ctx = await context(cliOptions)
+  ctx.watch()
 } else {
-  await build(options)
-
-  // Also build internals to expose
-  await build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    minify: true,
-    platform: 'node',
-    format: 'esm',
-    outfile: './dist/index.js',
-    define: { VERSION: `"${pkg.version}"` },
-  })
+  await build(cliOptions)
+  await build(indexOptions)
 }
