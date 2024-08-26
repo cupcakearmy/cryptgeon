ba474dff319968942f53196c11ce6710f3c42579
Niccolo Borgioli <hi@nicco.io>
2024-08-27 00:22:41 +0200

    more robust config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,13 +4,12 @@ const config: PlaywrightTestConfig = {
   use: {
     video: 'retain-on-failure',
     baseURL: 'http://localhost:3000',
-    actionTimeout: 10_000,
+    actionTimeout: 30_000,
   },
 
   outputDir: './test-results',
   testDir: './test',
   timeout: 30_000,
-  fullyParallel: true,
 
   webServer: {
     command: 'docker compose -f docker-compose.dev.yaml up',
