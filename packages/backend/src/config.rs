7915ca58b0af5affe7d21ab9f77d3413a8531b76
taschenuhr <29057942+taschenuhr@users.noreply.github.com>
2026-06-26 09:13:25 +0200

    allow base64 encoded theme images and remove the duplicated environment variable

--- packages/backend/src/config.rs
+++ packages/backend/src/config.rs
@@ -1,7 +1,8 @@
 use byte_unit::Byte;
 use std::sync::OnceLock;
 
-pub static THEME_SVG_CONTENT: OnceLock<String> = OnceLock::new();
+pub static THEME_RESOLVED_SVG: OnceLock<String> = OnceLock::new();
+pub static THEME_RESOLVED_IMAGE: OnceLock<String> = OnceLock::new();
 
 // Internal
 lazy_static! {
@@ -54,10 +55,6 @@ pub static ref IMPRINT_HTML: String = std::env::var("IMPRINT_HTML")
 // THEME
 lazy_static! {
     pub static ref THEME_IMAGE: String = std::env::var("THEME_IMAGE")
-        .unwrap_or("".to_string())
-        .parse()
-        .unwrap();
-    pub static ref THEME_SVG: String = std::env::var("THEME_SVG")
         .unwrap_or("".to_string())
         .parse()
         .unwrap();
